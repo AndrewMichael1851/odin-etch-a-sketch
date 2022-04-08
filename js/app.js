73b96b721f6e378de7e85ffd05b2c100ecc566aa
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-04-08 15:30:29 -0400

    Refactored v3.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-/******** Version 2.0 ********/
+/******** Version 3.0 ********/
+
 // Set the button IDs for reference later.
 const blkButton = document.getElementById('black-button');
 const rgbButton = document.getElementById('rainbow-button');
@@ -6,8 +7,8 @@ const eraButton = document.getElementById('eraser-button');
 const clrButton = document.getElementById('clear-button');
 
 // Set the size slider IDs for reference later.
-const gridSizeValue = document.getElementById('grid-size-selector-element');
-const gridSizeSlide = document.getElementById('grid-size-input');
+const gridSizeElement = document.getElementById('grid-size-element');
+const gridSizeInput = document.getElementById('grid-size-input');
 
 // Set the grid ID for reference later.
 const grid = document.getElementById('grid');
@@ -22,31 +23,50 @@ let currentColor = DEFAULT_COLOR;
 let currentState = DEFAULT_STATE;
 let currentSize = DEFAULT_SIZE;
 
-// Functions to change default values for the board.
+// Set the mouseDown event.
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+// Functions to change game state when certain buttons are pressed.
+blkButton.addEventListener('click', function (e) {
+    console.log(e);
+    setCurrentBoardState('black');
+});
+
+rgbButton.addEventListener('click', function (e) {
+    console.log(e);
+    setCurrentBoardState('rainbow');
+});
+
+eraButton.addEventListener('click', function (e) {
+    console.log(e);
+    setCurrentBoardState('eraser');
+});
+
+clrButton.addEventListener('click', function (e) {
+    console.log(e);
+    reloadGrid();
+});
+
+// Functions to update the status of the board.
 function setCurrentColor(newColor) {
     currentColor = newColor;
-    console.log('Color Switch: ' + newColor);
+    console.log('Color swtich: ' + newColor);
 }
 
-function setCurrentState(newState) {
+function setCurrentBoardState(newState) {
+    console.log('State switch: ' + currentState + ' to ' + newState);
     setButtonState(newState);
     currentState = newState;
-    console.log('State Switch: ' + newState);
 }
 
 function setCurrentSize(newSize) {
     currentSize = newSize;
-    console.log('Size Switch: ' + newSize);
+    console.log('Size switch: ' + newSize);
 }
 
-// Functions to change game state when certain buttons are pressed.
-blkButton.onclick = () => setCurrentState('black');
-rgbButton.onclick = () => setCurrentState('rainbow');
-eraButton.onclick = () => setCurrentState('eraser');
-
 // Functions to control the reset button.
-clrButton.onclick = () => reloadGrid();
-
 function reloadGrid() {
     clearGrid();
     setupGrid(currentSize);
@@ -56,23 +76,9 @@ function clearGrid() {
     grid.innerHTML = '';
 }
 
-// Functions to control the grid size.
-gridSizeSlide.onmousemove = (e) => updateSizeValue(e.target.value);
-gridSizeSlide.onchange = (e) => changeSize(e.target.value);
-
-function updateSizeValue(value) {
-    gridSizeValue.innerHTML = `Grid Size: ${value} x ${value}`;
-}
-
-function changeSize(value) {
-    setCurrentSize(value);
-    updateSizeValue(value);
-    reloadGrid();
-}
-
-// Change the toggle status of the buttons via CSS classes.
+// Function to change the button state CSS.
 function setButtonState(newState) {
-    // Remove the active state of current button.
+    // Remove the active state of the buttons.
     if (currentState === 'black') {
         blkButton.classList.remove('active');
     } else if (currentState === 'rainbow') {
@@ -84,32 +90,49 @@ function setButtonState(newState) {
     }
 
     // Add the active state to new button.
-    if (currentState === 'black') {
+    if (newState === 'black') {
         blkButton.classList.add('active');
-    } else if (currentState === 'rainbow') {
+    } else if (newState === 'rainbow') {
         rgbButton.classList.add('active');
-    } else if (currentState === 'eraser') {
+    } else if (newState === 'eraser') {
         eraButton.classList.add('active');
     } else {
         console.log(ERROR);
     }
 }
 
-// Setup the grid based on the values chosen.
-function setupGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+// Function to control the grid size settings.
+gridSizeInput.addEventListener('change', function (e) {
+    changeSize(e.target.value);
+});
 
-    for (var i = 0; i < size; i++) {
-        const gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
+function updateSizeValue(value) {
+    gridSizeElement.innerHTML = `Grid Size: ${value} x ${value}`;
+}
+
+function changeSize(value) {
+    setCurrentSize(value);
+    updateSizeValue(value);
+    reloadGrid();
+}
+
+// Setup the grid using defined settings.
+function setupGrid(gridSize) {
+    let cellSize = (40 / gridSize) + 'vw';
+
+    grid.style.setProperty('--grid-rows', gridSize);
+    grid.style.setProperty('--grid-columns', gridSize);
+    grid.style.setProperty('--cell-width', cellSize);
+    grid.style.setProperty('--cell-height', cellSize);
+
+    for (var i = 0; i < gridSize * gridSize; i++) {
+        let gridCell = document.createElement('div');
+        grid.appendChild(gridCell).className = 'grid-cell';
         gridCell.addEventListener('mouseover', changeColor);
         gridCell.addEventListener('mousedown', changeColor);
-        grid.appendChild(gridCell);
     }
 }
 
-// Change the color of individual cells.
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return;
     if (currentState === 'rainbow') {
@@ -117,39 +140,17 @@ function changeColor(e) {
         const randomG = Math.floor(Math.random() * 256);
         const randomB = Math.floor(Math.random() * 256);
         e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-    } else if (currentState === 'color') {
+    } else if (currentState === 'black') {
         e.target.style.backgroundColor = currentColor;
     } else if (currentState === 'eraser') {
         e.target.style.backgroundColor = '#fefefe';
+    } else {
+        console.log('ERROR');
     }
 }
 
-// Create the grid on window load.
-window.onLoad = () => {
+// Window load function calls.
+window.onload = () => {
     setupGrid(DEFAULT_SIZE);
     setButtonState(DEFAULT_STATE);
 }
-
-
-/******** Version 1.0 ********/
-/*
-const container = document.getElementById('grid');
-const gridSizePrompt = prompt('What grid size should we use?')
-const inputGridSize = document.querySelectorAll('.grid-size-range');
-
-function makeGrid(gridSize) {
-    let cellSize = (40 / gridSize) + 'vh';
-
-    container.style.setProperty('--grid-rows', gridSize);
-    container.style.setProperty('--grid-columns', gridSize);
-    container.style.setProperty('--cell-width', cellSize);
-    container.style.setProperty('--cell-height', cellSize);
-
-    for (var i = 0; i < (gridSize * gridSize); i++) {
-        let gridCell = document.createElement('div');
-        container.appendChild(gridCell).className = 'grid-cell';
-    }
-}
-
-makeGrid(gridSizePrompt);
-*/
